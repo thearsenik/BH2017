d067d50235b1e6234dcb53c92b9dcf28346450c3
TheArsenik <thearsenik@gmail.com>
2017-01-24 00:43:41 +0100

    Récupération des propositions (bug pour certaines descriptions)

--- crawler.js
+++ crawler.js
@@ -2,31 +2,52 @@ var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
 
-var START_URL = "https://www.benoithamon2017.fr/thematique/pour-un-progres-social-et-ecologique/";
+var urls = [
+  "https://www.benoithamon2017.fr/thematique/pour-un-progres-social-et-ecologique/",
+  "https://www.benoithamon2017.fr/thematique/pour-une-republique-bienveillante-et-humaniste/",
+  "https://www.benoithamon2017.fr/thematique/pour-une-france-independante-et-protectrice/"
+];
 
-var url = new URL(START_URL);
-var baseUrl = url.protocol + "//" + url.hostname;
+var nbResponse = 0;
+var allPropositions = [];
+urls.map(function(url){
+  return crawl(url);
+});
 
-pagesToVisit.push(START_URL);
-crawl();
-
-function crawl() {
-  request(url, function(error, response, body) {
+function crawl(url) {
+  return request(url, function(error, response, body) {
+    nbResponse++;
      if(response.statusCode === 200) {
        // Parse the document body
        var $ = cheerio.load(body);
-       var propositions = getAllPropositions($);
-       propositions.forEach(function(proposition){
-         console.log(proposition.title);
-       });
+       parseAllPropositions($);
+     }
+     if (nbResponse === urls.length) {
+       console.log(allPropositions);
      }
   });
 }
 
-function getAllPropositions($, word) {
-  var propositions = $('.Propositions > Propositions-Proposition');
-  propositions = propositions.map(function(element){
+function parseAllPropositions($, word) {
+  var propositions = $('.Propositions > .Propositions-Proposition');
+  propositions.each(function(index){
+    var element = propositions[index];
+    var obj = {};
+    obj.title = $(element).find(".flex-title").text();
 
+    // Si on a déjà récupéré cette proposition, on l'ignore
+    if (arrayContains(allPropositions, obj.title)) {
+      return;
+    }
+
+    obj.description = $(element).find(".ExtendedPropal > *:first-child > span").text();
+    allPropositions.push(obj);
   });
-  return propositions;
+}
+
+function arrayContains(array, elt) {
+  var filtered = array.filter(function(eltInArray){
+    return elt === eltInArray.title;
+  });
+  return filtered.length > 0;
 }
